383daadfac6c51644190c1374500dfa6ef2afd0b
Logandevries <lrdevrie@ucsc.edu>
2023-05-01 22:10:30 -0700

    lab 6 (fixed)

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -3,28 +3,36 @@
 // Created: 29 April
 
 // Define Variables
-myTransport =  ["walking", "Bus", "Uber"]
+var myTransport = ["Ford Taurus", "walking", "Bus", "Uber"];
 
 // Create an object for my main ride
 var myMainRide = {
-  make: "Ford",
-  model: "Taurus",
-  color: "Grey",
-  year: "2005",
-  age: function() {
-      return 2023 - this.year
-  }
+    make : "Ford",
+    model : "Taurus",
+    color : "Grey",
+    year : 2005,
+    age : function() {
+       return 2023 - this.year;
+    }
 }
 
 // output
-document.writeIn("Kinds of Transportation I use: ", myTransport, "</br>")
+document.writeln("Kinds of Transportation I use: ", myTransport, "</br>");
 // this little trick allows us to write an object to the document
-document.writeIn("My Main Ride: <pre>",
-  JSON.stringify(myMainRide, null, '/t'), "</pre>")
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
 
 
 
-  
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here

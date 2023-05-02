88677767f1b82baf38b1660827c56b152d7abe07
Logandevries <lrdevrie@ucsc.edu>
2023-05-01 18:02:10 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,30 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Logan de Vries
+// Created: 29 April
 
-// Constants
+// Define Variables
+myTransport =  ["walking", "Bus", "Uber"]
 
-// Functions
+// Create an object for my main ride
+var myMainRide = {
+  make: "Ford",
+  model: "Taurus",
+  color: "Grey",
+  year: "2005",
+  age: function() {
+      return 2023 - this.year
+  }
+}
 
+// output
+document.writeIn("Kinds of Transportation I use: ", myTransport, "</br>")
+// this little trick allows us to write an object to the document
+document.writeIn("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '/t'), "</pre>")
+
+
+
+  
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here

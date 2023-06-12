9542a1c6adbe5aac4e476334bc9f8d9588941a8c
Logandevries <lrdevrie@ucsc.edu>
2023-06-12 16:44:47 -0700

    fixing pushed

--- projProfileLogan/js/lab.js
+++ projProfileLogan/js/lab.js
@@ -5,20 +5,20 @@
 // Define Variables
 
 $(document).ready(function() {
-  // ...
-
-  // Click event for the search button
+  
   $('#search-button').on('click', function() {
     var apiKey = '7d06d4f6';
-    var movieTitle = $('#movie-input').val(); // Get the movie title from the input field
+    var movieTitle = $('#movie-input').val(); 
 
-    var apiUrl = 'http://www.omdbapi.com/?apikey=' + apiKey + '&t=' + encodeURIComponent(movieTitle);
+    var apiUrl = 'https://www.omdbapi.com/?apikey=' + apiKey + '&t=' + encodeURIComponent(movieTitle);
+
+    
 
     $.ajax({
       url: apiUrl,
       method: 'GET',
       success: function(response) {
-        // Handle the response data
+       
         console.log(response);
         $('#search-movie').html('<h2>' + response.Title + '</h2><p>' + response.Plot + '</p>');
       },
@@ -30,7 +30,7 @@ $(document).ready(function() {
 
 });
 
-  // Click event for the image buttons
+  
   $('#mini .image-button').on('click', function() {
     $(this).toggleClass('clicked');
     $(this).find('.description').toggle();
